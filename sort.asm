d6075839de7c8716908acd165e9bdd7109e7aeb4
sh0d4n <70346831+gubanovpm@users.noreply.github.com>
2021-10-04 13:24:48 +0300

    Update sort.asm

--- sort.asm
+++ sort.asm
@@ -5,7 +5,7 @@ extern scanf
 extern printf
 
 section .text
-main
+main:
 ;-----------------------------------------------------------------------
 ; считывание размера массива
 push dword n
@@ -16,7 +16,7 @@ add esp, 8
 ;-----------------------------------------------------------------------
 ; считываем массив
 mov ecx, [n]
-xor rsi, rsi
+xor esi, esi
 read_array:
     cmp esi, ecx
     je exit_read_array
@@ -34,32 +34,7 @@ exit_read_array:
 ;-----------------------------------------------------------------------
 ;-----------------------------------------------------------------------
 
-heapSort:
-    
-heapify:
-
-    mov eax, [i]    ; rax = largest
-
-    mov esi, [i]
-    sal esi, 1
-    inc esi         ; rsi - left
-    mov edi, [i]
-    sal edi, 1
-    add edi, 2      ; rdi - right
-    
-    cmp esi, [n]
-    jge heapify_1
-    
-    mov ebx, [array1 + 4 * esi] ; ebx = array1[left]
-    mov ecx, [array1 + 4 * eax] ; ecx = array1[largest]
-    
-    cmp ebx, ecx
-    jle heapify_1
-    
-    mov eax, esi    ; eax(= largest) = left
-    
-heapify_1:
-    
+heapSort:    
     
 
 ;-----------------------------------------------------------------------
@@ -70,22 +45,74 @@ mov ecx, [n]
 write_array:
     cmp esi, ecx
     je exit_write_array
-    
+
     mov eax, [array1 + 8 * esi]
 
     push eax
     push dword frmt_p
     call printf
     add esp, 8
-    
+
     inc rsi
 exit_write_array:
 ; конец вывода отсортированного массива
 ;-----------------------------------------------------------------------
 push dword 0
 call exit
- 
+
 ret
+;-----------------------------------------------------------------------
+heapify:
+
+    mov eax, [i]    ; rax = largest
+
+    mov esi, [i]
+    sal esi, 1
+    inc esi         ; rsi - left
+    mov edi, [i]
+    sal edi, 1
+    add edi, 2      ; rdi - right
+
+    cmp esi, [n]
+    jge heapify_1
+
+    mov ebx, [array1 + 4 * esi] ; ebx = array1[left]
+    mov ecx, [array1 + 4 * eax] ; ecx = array1[largest]
+
+    cmp ebx, ecx
+    jle heapify_1
+    mov eax, esi    ; eax(= largest) = left
+
+heapify_1:
+
+    cmp edi, [n]
+    jge heapify_2
+
+    mov ecx, [array1 + 4 * eax] ; ecx = array1[largest]
+    mov edx, [array1 + 4 * edi] ; edi = array1[right]
+
+    cmp edx, ecx
+    jle heapify_2
+    mov eax, edi
+
+heapify_2:
+    mov ecx, [i]
+    cmp eax, ecx
+    je exit_heapify
+    
+    mov edi, [array1 + 4 * ecx]     ; edi = array1[i]
+    mov esi, [array1 + 4 * eax]     ; esi = array1[largest]
+    ; swap(array1[i], arr1[largest])
+    mov [array1 + 4 * ecx], esi
+    mov [array1 + 4 * eax], edx
+    
+    mov [i], eax
+    call heapify
+
+exit_heapify:
+    ret
+        
+    
 
 section .data
 frmt_s : db '%d', 0
